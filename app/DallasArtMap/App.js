08075835bc13d7c81b6e5d76b7b7eba8d83806a5
Brendan Honea <brendan@Brendans-MacBook-Pro.local>
2018-08-15 23:38:00 -0500

    - Art Co marker displaying on map
    -menu button

--- app/DallasArtMap/App.js
+++ app/DallasArtMap/App.js
@@ -1,46 +1,71 @@
- import React, { Component } from 'react'
- import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
- import { View, StyleSheet } from 'react-native'
+import React, { Component } from "react";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
+import {
+  View,
+  StyleSheet,
+  Alert,
+  TouchableHighlight,
+  Image,
+  Text
+} from "react-native";
 
 export default class App extends Component<Props> {
-  render () {
+  _onPressButton() {
+    Alert.alert("works");
+  }
+
+  render() {
     return (
       <View style={styles.container}>
         <MapView
           provider={PROVIDER_GOOGLE}
           style={styles.map}
           region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
+            latitude: 32.78428,
+            longitude: -96.777388,
             latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
+            longitudeDelta: 0.0121
           }}
-        ></MapView>
+        >
+          <Marker coordinate={artCo} title={"Deep Ellum Art Co"} />
+        </MapView>
+          <TouchableHighlight
+            onPress={this._onPressButton}
+            underlayColor="white"
+          >
+            <Image source={require("./assets/menu2.png")} />
+          </TouchableHighlight>
       </View>
-    )
+    );
   }
 }
 
+const artCo = {
+  latitude: 32.78428,
+  longitude: -96.777388
+};
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
   map: {
-    flex: 1,
+    ...StyleSheet.absoluteFillObject
   },
+  menuView: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%"
+  },
+  menuButton: {
+    // backgroundColor: "black",
+    // flexDirection: "row",
+    // flex: 1
+    // paddingTop: 10,
+    // paddingLeft: 10
+    // width: '60px',
+    // marginLeft: 15,
+  }
 });
-
-// const styles = StyleSheet.create({
-//   container: {
-//     borderRadius: 4,
-//     borderWidth: 0.5,
-//     borderColor: '#d6d7da',
-//   },
-//   title: {
-//     fontSize: 19,
-//     fontWeight: 'bold',
-//   },
-//   activeTitle: {
-//     color: 'red',
-//   },
-// });
